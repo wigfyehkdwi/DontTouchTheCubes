caafc177d85a2945de4e23a2c25ad0fd6066d8a2
wigfyehkdwi <amoigus42069@hotmail.com>
2023-05-06 12:55:37 -0400

    oohio

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SpawnObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public GameObject target;
+    public float difficulty = 40f;
 
     // Update is called once per frame
     void Update()
     {
+        int spawnCount = (int) (difficulty * Time.deltaTime);
+        difficulty = Time.deltaTime * 4f;
 
+        while (spawnCount > 0) {
+          Vector3 position = transform.position + new Vector3(Random.value * 40f - 20f, 0f, Random.value * 40f - 20f);
+          Quaternion rotation = Quaternion.Euler(0f, Random.value * 360f, Random.value * 30f);
+          Instantiate(target, position, rotation);
+
+          spawnCount--;
+        }
     }
 }
