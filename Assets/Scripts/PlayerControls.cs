1a00ac5fc79ed9bd3e753555527c6a234828ab02
wigfyehkdwi <amoigus42069@hotmail.com>
2023-05-06 12:28:54 -0400

    scripting in ohio

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,15 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.rotation *= Quaternion.Euler(0, 0, 7 * Time.deltaTime);
+        Time.timeScale += Time.fixedDeltaTime * 0.01f;
+        rb.velocity += transform.rotation * (Vector3.up * Input.GetAxisRaw("Vertical") * 10f * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -30f, 30f), transform.position.y, Mathf.Clamp(transform.position.z, -30f, 30f));
     }
 }
