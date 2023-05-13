2b82c02790bae155c664631e08da5f2103c2fa0f
wigfyehkdwi <amoigus42069@hotmail.com>
2023-05-13 11:33:36 -0400

    add GEESE
    very important change ngl
    free no vuris
    work 2034

--- Assets/Scripts/MovingAndRotatingObjects.cs
+++ Assets/Scripts/MovingAndRotatingObjects.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class MovingAndRotatingObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 50f;
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
     }
 }
